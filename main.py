eafa09255431dec3a946cb48d368decf6b2a501f
Gautam <gautamtemp418@gmail.com>
2025-04-20 08:31:44 +0530

    Fix: using  0.0.0.0 for Railway port binding

--- main.py
+++ main.py
@@ -122,4 +122,4 @@ def verify_access(token: str, request: Request, db: Session = Depends(get_db)):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="127.0.0.1", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
